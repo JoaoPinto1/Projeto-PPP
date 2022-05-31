dfe558b26af2f7d95344f3e891acc4553a2e4f2d
JoaoPinto1 <94558083+JoaoPinto1@users.noreply.github.com>
2022-05-31 18:42:39 +0100

    Add files via upload

---
+++ Parte 1/header.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "lib-utf8.h"
+
+#define MAXNOME 50
+
+int ler_fich(char nome_fich[]);
